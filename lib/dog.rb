917b7f6067f9be0adb110cf07a495ee075c9c588
Joana Palero <joana.palero24@gmail.com>
2018-06-25 21:42:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog 
   def name=(dogs_name)
-    @this_dogs_name = dog_name
+    @this_dogs_name = dogs_name
   end
   
   def name
@@ -8,3 +8,7 @@ class Dog
   end
 end
 
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+puts lassie.name
